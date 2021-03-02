f68139cecb944720e5dcb7c538840db4caaffcc6
Diogo Valente de Aragão <diogovalentearagao@outlook.com>
2021-03-02 20:15:11 -0300

    league v4 implemented

--- LeagueApi.php
+++ LeagueApi.php
@@ -4,7 +4,7 @@
 class SummonerApi {
 
     private $baseUrl = 'https://br1.api.riotgames.com/lol/';
-    private $apiKey = 'RGAPI-02eb5e3d-e5c1-4368-8823-9f5d2dd90204';
+    private $apiKey = 'YOUR_API_KEY';
 
     public function getSummonerByName($summonerName) { // SUMMONER-V4 api
 
