e6e478fc4e472d421c4733e6c8d956940964f094
Diogo Valente de Aragão <diogovalentearagao@outlook.com>
2021-03-02 19:54:28 -0300

    finished League api V4

--- LeagueApi.php
+++ LeagueApi.php
@@ -3,12 +3,12 @@
 
 class SummonerApi {
 
-    private $baseUrl = 'https://br1.api.riotgames.com/lol/summoner/v4/summoners/by-name/';
-    private $apiKey = 'RGAPI-02eb5e3d-e5c1-4368-8823-9f5d2dd90204';
+    private $baseUrl = 'https://br1.api.riotgames.com/lol/';
+    private $apiKey = 'YOUR_API_KEY';
 
-    public function getSummonerByName($summonerName) {
+    public function getSummonerByName($summonerName) { // SUMMONER-V4 api
 
-        $url = $this->baseUrl.$summonerName.'?api_key='.$this->apiKey;
+        $url = $this->baseUrl.'summoner/v4/summoners/by-name/'.$summonerName.'?api_key='.$this->apiKey;
 
         $ch = curl_init($url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
@@ -21,11 +21,21 @@ class SummonerApi {
 
     }
 
-    public function
+    public function getLeagueById($summonerId){ // LEAGUE-V4 apí | return all information like tier, rank, wins, losses, lp etc...
+
+        $url = $this->baseUrl.'league/v4/entries/by-summoner/'.$summonerId.'?api_key='.$this->apiKey;
+
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+        $response = curl_exec($ch);
+        curl_close($ch);
+        $data = json_decode($response, true);
+
+        return $data;
+
+    }
 
 }
-
-
-
 
 ?>
